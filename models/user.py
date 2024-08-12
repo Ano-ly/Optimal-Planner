c2562bb6052e92adaac98caa8d20e229b3b90cb0
fortune-0 <fortunepeterspc07@gmail.com>
2024-08-12 05:47:37 +0100

    Create the user table in the database

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" User class """
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True);
+    username = Column(String(200), unique=True, nullable=False);
+    password = Column(String(255), nullable=False);
+    email = Column(String(255), unique=True, nullable=False);
+    created_at = Column(DateTime, default=datetime.utcnow);
