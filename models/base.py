23094ae5ba02e3f703994cb9bbfee09c2f6260b0
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-12 05:12:26 +0100

    Create base class

---
+++ models/base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Base object; declarative base"""
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """Base class"""
+
+    pass
---
+++ models/event.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+"""The event object"""
+
+
