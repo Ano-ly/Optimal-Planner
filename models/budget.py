b2eaeb60d27c5ba1a80ba45baa7b15ddb46284cd
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-14 18:23:06 +0100

    Create new classes mapped to sql tables

---
+++ models/budget.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Budget class"""
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from typing import List
+from models.base import Base
+
+
+class Budget(Base):
+
+    """Class Budget mapped to the budget table"""
+    __tablename__ = "budget"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    total: Mapped[int] = mapped_column(nullable=False)
+
+    budget_items: Mapped[List["BudgetItem"]] = relationship(
+        back_populates="budget", cascade="all, delete-orphan")
+
+    event_id: Mapped[int] = mapped_column(ForeignKey("event.id"),
+        nullable=False)
+    event: Mapped["Event"] = relationship(back_populates="budget")
---
+++ models/budget_items.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""BudgetItem class"""
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from models.base import Base
+
+
+class BudgetItem(Base):
+
+    """Class Budget mapped to the budget table"""
+    __tablename__ = "budgetitem"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    desc: Mapped[str] = mapped_column(String)
+    total: Mapped[int] = mapped_column(nullable=False)
+
+    budget_id: Mapped[int] = mapped_column(ForeignKey("budget.id"),
+        nullable=False)
+    budget: Mapped["Budget"] = relationship(back_populates="budge_items")
