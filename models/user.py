aa84130a0edc9a5070ce93425f14b96379c71d71
fortune-0 <fortunepeterspc07@gmail.com>
2024-08-14 09:17:00 +0100

    Updated

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ User class """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Integer, String, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, declarative_base
+from .base import Base
 from datetime import datetime
 
 Base = declarative_base()
@@ -9,9 +10,9 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     
-    id: Mapped[int] = mapped_column(Integer, primary_key=True);
-    username: Mapped[str] = mapped_column(String(200), unique=True, nullable=False);
-    password: Mapped[str] = mapped_column(String(255), nullable=False);
-    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False);
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow);
-    update_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, onUpdate=datetime.utcnow);
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(200), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
