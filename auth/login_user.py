02254ab060ceb23715aa8a0401bf1acdff27cfc6
fortune-0 <fortunepeterspc07@gmail.com>
2024-08-15 03:02:56 +0100

    Modified the Auth_user function

--- auth/login_user.py
+++ auth/login_user.py
@@ -5,30 +5,28 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.user import User
+import getpass
 import bcrypt
 
-def authenticate_user(db: Session, username: str, password: str):
+def authenticate_user(db: Session, identify: str, password: str):
     """Check if a user exists and if their password matches"""
-    user = db.query(user.User).filter(user.User.username == username).first()
+    if is_valid(identify):
+        user = db.query(User).filter(User.username == identify).first()
+    else:
+        user = db.query(User).filter(User.email == identify).first()
     if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
         return user
     return None
 
-def login_user(db: Session, username: str, email: str, password: str):
-    """ """
-    user = authenticate_user(db, username, password)
+def login_user(db: Session, identify: str, password: str):
+    """ Logs user in with either Username or email """
+    user = authenticate_user(db, identify, password)
     
     try:
         if user:
-            if user.email == valid_email:
-                if is_valid_email(email):
-                        return "Login successful!"
-                else:
-                    return "Invalid email format."
-            else:
-                    return "Email or password is incorrect."
+            return "Login successful!"
         else:
-            return "Username does not exist."
+            return "Email/Username or password is incorrect."
     except Exception as e:
         print(f"An error occurred: {e}")
         return "An error occurred during login."
