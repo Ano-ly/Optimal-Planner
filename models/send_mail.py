017a17d01249b7883247e3047f339a5e59ebb21e
fortune-0 <fortunepeterspc07@gmail.com>
2024-08-17 05:52:21 +0100

    Implemeted the login to gmail function, - Added the send mail function

---
+++ models/send_mail.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from models.invitee import Invitee
+from sqlalchemy.orm import Session
+import smtplib
+""" """
+
+session = Session()
+
+def login_gmail(email_id: str, email_id_password: str):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(email_id, email_id_password)
+        return("Login successful")
+        return server
+    except Exception as e:
+        return(f"Failed to login: {e}")
+
+def logout_gmail(server):
+    try:
+        server.quit()
+        return("Logout successful")
+    except Exception as e:
+        return(f"Failed to logout: {e}")
+
+def send_mail(server, email_id: str):
+    invitee_emails = session.query(Invitee.email).all()
+    for dest in invitee_emails:
+        message = "message"
+        try:
+            server.sendmail(email_id, dest.email, message)
+            return(f"Email sent to {dest.email}")
+        except Exception as e:
+            return(f"Failed to send email to {dest.email}: {e}")
+
+    session.close()
