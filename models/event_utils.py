333b453f71ed08469caf99ef90e180015e259d48
fortune-0 <fortunepeterspc07@gmail.com>
2024-08-20 14:46:37 +0100

    Update the method call

--- models/event_utils.py
+++ models/event_utils.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+""" Retrives event location from the database
+and returns a google maps link to the location """
 from models.event import Event
 from sqlalchemy.orm import Session
 
@@ -5,7 +8,7 @@ def get_event_map(session: Session, event_id: int) -> str:
     event = session.query(Event).filter_by(id=event_id).first()
     if event and event.location:
         base_url = "https://www.google.com/maps/search/?api=1&query="
-        map_link = base_url + event.location.replace
+        map_link = base_url + event.location.replace(" ", "+" )
         return map_link
     else:
         raise ValueError("Location not set")
