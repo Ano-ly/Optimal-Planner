7dd46763db49de3b79ec20734b6c06fd52b1bde2
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-22 05:27:04 +0100

    Move delete function to base file

--- models/base.py
+++ models/base.py
@@ -7,4 +7,17 @@ from sqlalchemy.orm import DeclarativeBase, Session
 class Base(DeclarativeBase):
     """Base class"""
 
-    pass
+    @classmethod
+    def delete_obj(cls, session: Session, obj_id: int) -> None:
+        """Delete an object from the database"""
+
+        try:
+            obj = session.query(cls).filter_by(id=obj_id).one_or_none()
+            if not obj:
+                raise Exception("Item does not exist")
+            session.delete(obj)
+            session.commit()
+            return (None)
+        except Exception as e:
+            session.rollback()
+            raise Exception(f"An error occured: {e}")
