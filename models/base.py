46ec431ed52a8fe7fe9276cbaf35ce45290b5c8c
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-22 05:11:46 +0100

    Implemet delete function for objects

--- models/base.py
+++ models/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Base object; declarative base"""
 
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Session
 
 
 class Base(DeclarativeBase):
