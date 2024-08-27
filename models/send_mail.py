edf2b6ebec8fa96dd6df126e4461799efc0c483d
fortune-0 <fortunepeterspc07@gmail.com>
2024-08-27 05:25:39 +0100

    Refactor email sending functionality and update email retrieval query

--- models/send_mail.py
+++ models/send_mail.py
@@ -6,7 +6,7 @@ This module provides functions to send emails to guests of an event using a Gmai
 It uses SQLAlchemy to interact with the database and retrieve guest email addresses."""
 
 
-from models.event import Event
+from models.invite import Invite
 from sqlalchemy.orm import Session
 import smtplib
 
@@ -43,7 +43,7 @@ def logout_gmail(server):
     except Exception as e:
         return(f"Failed to logout: {e}")
 
-def send_mail(server, email_id: str):
+def send_mail(server, email_id: str, event_id: int, session: Session):
     """
     This function sends an email to all guests of an event using an SMTP server.
 
@@ -57,7 +57,7 @@ def send_mail(server, email_id: str):
     Raises:
     Exception: If an error occurs while sending an email to a guest, an exception is raised with a descriptive error message.
     """
-    invitee_emails = session.query(Event.guest.email).all()
+    invitee_emails = session.query(Invite.email).filter_by(event_id=event_id).all()
     for dest in invitee_emails:
         message = "Message."
         try:
