2ad399c01eb4da0fa03d32c8828d457a96dbe601
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-27 21:02:03 +0100

    Edit files to prevent merge conflict

---
+++ frontend/static/js/index.js
@@ -0,0 +1,9 @@
+function loginPage () {
+  window.location.href = "/login";
+}
+function aboutPage () {
+  window.location.href = "/about";
+}
+function contactPage () {
+  window.location.href = "/contact";
+}
